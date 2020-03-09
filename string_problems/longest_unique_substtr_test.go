8ffc8474177eacf46a51a9f9774040b7fbe1ebd9
Anh Nguyen Phi <anh.nguyenphi@grabtaxi.com>
2020-03-09 11:02:12 +0700

    string problems

--- string_problems/longest_unique_substtr_test.go
+++ string_problems/longest_unique_substtr_test.go
@@ -1,3 +1,40 @@
 package string_problems
 
+import (
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
 // https://www.geeksforgeeks.org/length-of-the-longest-substring-without-repeating-characters/
+
+func longestUniqueSubsttr(s string) int {
+	maxLen := 0
+	curLen := 0
+	visited := map[byte]int{}
+
+	for i := 0; i < len(s); i++ {
+		lastIdx, ok := visited[s[i]]
+
+		if !ok || i-curLen > lastIdx {
+			curLen++
+		} else {
+			if curLen > maxLen {
+				maxLen = curLen
+			}
+			curLen = i - lastIdx
+		}
+
+		visited[s[i]] = i
+	}
+
+	return maxLen
+}
+
+func TestLongestUniqueSubsttr1(t *testing.T) {
+	assert.Equal(t, longestUniqueSubsttr("ABDEFGABEF"), 6)
+}
+
+func TestLongestUniqueSubsttr2(t *testing.T) {
+	assert.Equal(t, longestUniqueSubsttr("GEEKSFORGEEKS"), 7)
+}
